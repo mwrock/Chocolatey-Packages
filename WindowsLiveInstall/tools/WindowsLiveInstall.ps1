e557432aff56e63ff46ad9204f06a528bde1c037
Matt Wrock <matt@mattwrock.com>
2012-04-15 09:20:05 -0700

    replace single quotes with double quotes in installer and remove versions from nuspec file names

--- WindowsLiveInstall/tools/WindowsLiveInstall.ps1
+++ WindowsLiveInstall/tools/WindowsLiveInstall.ps1
@@ -8,7 +8,7 @@ try {
     if (!(Test-Path $tempDir)) {New-Item $tempDir -type Directory}
     $file = Join-Path $tempDir "wlsetup-all.exe"
     if(!(Test-Path $file)) { Get-ChocolateyWebFile WindowsLive $file http://g.live.com/1rewlive4-all/en/wlsetup-all.exe }
-    Install-ChocolateyInstallPackage 'WindowsLiveMesh' 'exe' '/q /NOToolbarCEIP /NOhomepage /Nolaunch /nosearch /AppSelect:$product' $file
+    Install-ChocolateyInstallPackage "WindowsLiveMesh" 'exe' "/q /NOToolbarCEIP /NOhomepage /Nolaunch /nosearch /AppSelect:$product" $file
     Write-ChocolateySuccess $package
 } catch {
     Write-ChocolateyFailure $package $($_.Exception.Message)
