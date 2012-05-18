f02fe58f77487d62941511abf454219dd27bbb40
Matt Wrock <matt@mattwrock.com>
2012-05-18 01:37:37 -0700

    fixed most bugs but hg tab completion is not working

--- Posh-GIT-HG/tools/ChocolateyInstall.ps1
+++ Posh-GIT-HG/tools/ChocolateyInstall.ps1
@@ -1,46 +1,29 @@
 # Adapted from http://www.west-wind.com/Weblog/posts/197245.aspx
-function Get-FileEncoding($Path) {
-    $bytes = [byte[]](Get-Content $Path -Encoding byte -ReadCount 4 -TotalCount 4)
-
-    if(!$bytes) { return 'utf8' }
-
-    switch -regex ('{0:x2}{1:x2}{2:x2}{3:x2}' -f $bytes[0],$bytes[1],$bytes[2],$bytes[3]) {
-        '^efbbbf'   { return 'utf8' }
-        '^2b2f76'   { return 'utf7' }
-        '^fffe'     { return 'unicode' }
-        '^feff'     { return 'bigendianunicode' }
-        '^0000feff' { return 'utf32' }
-        default     { return 'ascii' }
-    }
-}
-
-function Add-Line([string]$profileLine) {
-    Write-Host "Adding $profileLine to profile..."
-    @"
-
-    $profileLine
-
-"@ | Out-File $PROFILE -Append -Encoding (Get-FileEncoding $PROFILE)
-}
-
-function Check-Profile([string] $profileLine) {
-    if(!(Select-String -Path $PROFILE -Pattern $profileLine -Quiet -SimpleMatch)) {
-        Add-Line $profileLine
-    }
-}
-
-
 try {
+    $oldProfile = [string[]](Get-Content $PROFILE)
+    $newProfile = @()
     $lib = (Split-Path -parent (Split-Path -parent (Split-Path -parent $MyInvocation.MyCommand.Definition)))
     $oldPhgProf = ". '$lib\posh-git-hg.1.0.1\profile.example.ps1'"
-    (Get-Content $PROFILE) | ForEach-Object {
-        % {$_ -replace $oldPhgProf, "" }
-    } | Set-Content $PROFILE
-
+    $removePoshHGPrompt = "function Prompt() {if(Test-Path Function:\PrePoshHGPrompt){PrePoshHGPrompt}}"
+    foreach($line in $oldProfile) {
+        $line = $line.replace($oldPhgProf, "")
+        $line = $line.replace($removePoshHGPrompt, "")
+        if($line.Trim().Length -gt 0) {
+            $newProfile += $line
+        }        
+    }
+    $newProfile += $removePoshHGPrompt
+    Set-Content -path $profile  -value $newProfile -Force
     Write-Host 'Please reload your profile for the changes to take effect:'
     Write-Host '    . $PROFILE'
     Write-ChocolateySuccess 'posh-git-hg'
 } catch {
+  try {
+    if($oldProfile){
+        Set-Content -path $PROFILE -value $oldProfile -Force
+    }
+  }
+  catch{}
   Write-ChocolateyFailure 'posh-git-hg' $($_.Exception.Message)
   throw
 }
