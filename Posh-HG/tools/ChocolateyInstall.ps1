59bf90a2901d6279d27e7022a7422d840ef55b42
Matt Wrock <matt@mattwrock.com>
2012-05-20 08:54:04 -0700

    works with ps3

--- Posh-HG/tools/ChocolateyInstall.ps1
+++ Posh-HG/tools/ChocolateyInstall.ps1
@@ -5,6 +5,8 @@ try {
     }
 
     $tools = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    $emptyTabExpansion = "if(!(Test-Path function:\TabExpansion)) { New-Item function:\TabExpansion -value '' | out-null }"
+
     if($oldProfile) {
         $newProfile = @()
         $lib = (Split-Path(Split-Path $tools -parent) -parent)
@@ -13,12 +15,14 @@ try {
         $newPrompt = "function Prompt() {if(Test-Path Function:\PrePoshHGPrompt){PrePoshHGPrompt}PoshHGPrompt}"
         foreach($line in $oldProfile) {
             foreach($path in (([array](dir $lib\posh-hg.*)))) {
+                write-host "ps3: $path\tools\profile.example-ps3.ps1"
                 $line = $line.replace(". '$path\tools\profile.example.ps1'", "")
                 $line = $line.replace(". '$path\tools\profile.example-ps3.ps1'", "")
             }
             $line = $line.replace($promptCheck, "")
             $line = $line.replace($poshPromptRename, "")
             $line = $line.replace($newPrompt, "")
+            $line = $line.replace($emptyTabExpansion, "")
             if($line.Trim().Length -gt 0) {
                 $newProfile += $line
             }
@@ -26,8 +30,21 @@ try {
         $newProfile += $promptCheck
         Set-Content -path $profile  -value $newProfile -Force
     }
+    
+    if(!(Get-Command hg -ErrorAction SilentlyContinue)) {
+        if( test-path "$env:programfiles\Mercurial" ) {$mPath="$env:programfiles\Mercurial"} else { $mPath = "${env:programfiles(x86)}\Mercurial" }
+        Install-ChocolateyPath $mPath
+    }
 
-    Install-ChocolateyZipPackage 'posh-hg' 'c:\dev\posh-hg.zip' $tools
+    Install-ChocolateyZipPackage 'posh-hg' '\\vboxsvr\autobox\posh-hg.zip' $tools
+
+    if(!(Test-Path $PROFILE)) {
+        Write-Host "Creating PowerShell profile...`n$PROFILE"
+        New-Item $PROFILE -Force -Type File
+    }
+    $newProfile = [string[]](Get-Content $PROFILE)
+    $newProfile += $emptyTabExpansion
+    Set-Content -path $profile  -value $newProfile -Force
 
     $install = (Get-Item "$tools\*\install.ps1")
     & $install
