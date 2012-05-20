99d3d5788ceb486ae7f01bac184b56b08db4c1cc
Matt Wrock <matt@mattwrock.com>
2012-05-20 13:40:03 -0700

    upgrades mostly working except for profile.example

--- clean.ps1
+++ clean.ps1
@@ -1,6 +1,6 @@
-$dirBase = (split-Path (Resolve-Path *.nupkg) -leaf).Replace(".nupkg","")
+$dirBase = (split-Path (Resolve-Path *.nupkg)[-1] -leaf).Replace(".nupkg","")
 $packageName = (split-Path (Resolve-Path *.nuspec) -leaf).Replace(".nuspec","")
-Remove-Item $env:Temp\Chocolatey\$dirBase* -Recurse -Force
+Remove-Item $env:Temp\Chocolatey\$packageName* -Recurse -Force
 Remove-Item $env:ChocolateyInstall\lib\$dirBase* -Recurse -Force
 $scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 #Copy-Item $scriptPath\Microsoft.PowerShell_profile.ps1 $profile -Force
