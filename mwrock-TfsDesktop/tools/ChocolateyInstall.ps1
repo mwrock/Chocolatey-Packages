e31607d7e7863193355f91c7928bcec5218bf142
Matt Wrock <matt@mattwrock.com>
2012-09-20 13:28:33 -0700

    changing package to work with new boxstarter/chocolatey

--- mwrock-TfsDesktop/tools/ChocolateyInstall.ps1
+++ mwrock-TfsDesktop/tools/ChocolateyInstall.ps1
@@ -5,66 +5,66 @@ try {
 
     Install-WindowsUpdate
 
-    Add-PersistentEnvVar "Bootstr_TemplateWorkspace" "wrockdesk_template"
+    Install-ChocolateyEnvironmentVariable "Bootstr_TemplateWorkspace" "wrockdesk_template"
 
     #Stock TFS dev environment
     & \\cpvsbuild\DROPS\dev11\Q11W\raw\current\binaries.x86ret\bin\i386\TfsBootstraper\build.bat
     
-    Install-FromChocolatey git-credential-winstore
+    cinst git-credential-winstore
 
-    Install-FromChocolatey console-devel
-    Set-PinnedApplication -Action PinToTaskbar -FilePath "$env:programfiles\console\console.exe"
+    cinst console-devel
+    Install-ChocolateyPinnedTaskBarItem "$env:programfiles\console\console.exe"
     copy-item (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'console.xml') -Force $env:appdata\console\console.xml
 
     Set-TaskbarSmall
     Enable-RemoteDesktop
-    Enable-Telnet
-    Enable-HyperV
-    Set-PinnedApplication -Action PinToTaskbar -FilePath "$env:windir\system32\mstsc.exe"
+    cinst TelnetClient -source windowsFeatures
+    cinst Microsoft-Hyper-V -source windowsFeatures
+    cinst Microsoft-Hyper-V-Management-Clients -source windowsFeatures
+    Install-ChocolateyPinnedTaskBarItem "$env:windir\system32\mstsc.exe"
 
-    Install-FromChocolatey sublimetext2
+    cinst sublimetext2
     $sublimeDir = (Get-ChildItem $env:systemdrive\chocolatey\lib\sublimetext* | select $_.last)
     $sublimeExe = "$sublimeDir\tools\sublime_text.exe"
     copy-item (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'sublime\*') -Force -Recurse "$sublimeDir\tools\data"
     move-item (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) "$sublimeDir\tools\data\Pristine Packages") -Force "$sublimeDir\tools"    
-    Set-PinnedApplication -Action PinToTaskbar -FilePath $sublimeExe
-    Add-ExplorerMenuItem "sublime" "Open with Sublime Text 2" $sublimeExe
-    Add-ExplorerMenuItem "sublime" "Open with Sublime Text 2" $sublimeExe "directory"
-    Set-FileAssociation ".txt" $sublimeExe
-    cmd /c assoc .=txtfile
+    Install-ChocolateyPinnedTaskBarItem $sublimeExe
+    Install-ChocolateyExplorerMenuItem "sublime" "Open with Sublime Text 2" $sublimeExe
+    Install-ChocolateyExplorerMenuItem "sublime" "Open with Sublime Text 2" $sublimeExe "directory"
+    Install-ChocolateyFileAssociation ".txt" $sublimeExe
+    Install-ChocolateyFileAssociation "." txtfile
 
-    Install-FromChocolatey skydrive
-    Install-FromChocolatey fiddler
-    Install-FromChocolatey winrar
-    Install-FromChocolatey posh-git-hg
-    Install-FromChocolatey tortoisehg
-    Install-FromChocolatey Paint.net
-    Install-FromChocolatey windirstat
-    Install-FromChocolatey sysinternals
-    Install-FromChocolatey evernote
-    Install-FromChocolatey NugetPackageExplorer
-    Install-FromChocolatey PowerGUI
-    Install-FromChocolatey InputDirector
-    Install-FromChocolatey QTTabBar
-    Install-FromChocolatey Firefox
-    Install-FromChocolatey windbg    
+    cinst skydrive
+    cinst fiddler
+    cinst winrar
+    cinst posh-git-hg
+    cinst tortoisehg
+    cinst Paint.net
+    cinst windirstat
+    cinst sysinternals
+    cinst evernote
+    cinst NugetPackageExplorer
+    cinst PowerGUI
+    cinst InputDirector
+    cinst QTTabBar
+    cinst Firefox
+    cinst windbg    
 
-    Install-FromChocolatey googlechrome
-    Set-PinnedApplication -Action PinToTaskbar -FilePath "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
+    cinst  googlechrome
+    Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
 
+    Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
+    cinst resharper
+    cinst TestDriven.Net
 
-    Set-PinnedApplication -Action PinToTaskbar -FilePath "${env:ProgramFiles(x86)}\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
-    Install-FromChocolatey resharper
-    Install-FromChocolatey TestDriven.Net
-
-    Install-FromChocolatey dotpeek
+    cinst dotpeek
     $dotPeekDir = (Get-ChildItem $env:systemdrive\chocolatey\lib\dotpeek* | select $_.last)
-    Set-FileAssociation ".dll" "$dotPeekDir\tools\dotPeek.exe"
+    Install-ChocolateyFileAssociation".dll" "$dotPeekDir\tools\dotPeek.exe"
 
-    Add-PersistentEnvVar "devFolder" "d:\dev"
+    Install-ChocolateyEnvironmentVariable "devFolder" "d:\dev"
     Install-ChocolateyPath "C:\Chocolatey\chocolateyInstall" "Machine"
 
-    Install-FromChocolatey autohotkey_l
+    cinst autohotkey_l
     $ahk = "$env:appdata\Microsoft\Windows\Start Menu\Programs\startup\AutoScript.ahk"
     set-content $ahk -Force -value @"
 ^+C::
