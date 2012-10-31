6f0ec84406371bfc21723f7ab2fac11aad59fcc3
Matt Wrock <matt@mattwrock.com>
2012-10-30 22:37:52 -0700

    more retrofitting of work setup to tfsbootstrapper and chocolatey recent features

--- mwrock-TfsDesktop/tools/ChocolateyInstall.ps1
+++ mwrock-TfsDesktop/tools/ChocolateyInstall.ps1
@@ -5,14 +5,10 @@ try {
 
     Install-WindowsUpdate
 
-    Install-ChocolateyEnvironmentVariable "Bootstr_TemplateWorkspace" "wrockdesk_template"
-
     #Stock TFS dev environment
-    & \\cpvsbuild\DROPS\dev11\Q11W\raw\current\binaries.x86ret\bin\i386\TfsBootstraper\build.bat
+    #& \\vscsstor\Tools\tfsbootstrapper\build full-install -sourcePath d:\dev\wit\src -binariesPath c:\dev\wit\binaries -branch $/devdiv/feature/q11w_mwrock2
+    #& \\vscsstor\Tools\tfsbootstrapper\build create-workspace -sourcePath d:\dev\wit\q11w -binariesPath c:\dev\wit\q11w\binaries -branch $/devdiv/d11rel/q11w
     
-    cinst git-credential-winstore
-
-    cinst console-devel
     Install-ChocolateyPinnedTaskBarItem "$env:programfiles\console\console.exe"
     copy-item (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'console.xml') -Force $env:appdata\console\console.xml
 
@@ -23,7 +19,6 @@ try {
     cinst Microsoft-Hyper-V-Management-Clients -source windowsFeatures
     Install-ChocolateyPinnedTaskBarItem "$env:windir\system32\mstsc.exe"
 
-    cinst sublimetext2
     $sublimeDir = (Get-ChildItem $env:systemdrive\chocolatey\lib\sublimetext* | select $_.last)
     $sublimeExe = "$sublimeDir\tools\sublime_text.exe"
     copy-item (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'sublime\*') -Force -Recurse "$sublimeDir\tools\data"
@@ -34,37 +29,15 @@ try {
     Install-ChocolateyFileAssociation ".txt" $sublimeExe
     Install-ChocolateyFileAssociation "." txtfile
 
-    cinst skydrive
-    cinst fiddler
-    cinst winrar
-    cinst posh-git-hg
-    cinst tortoisehg
-    cinst Paint.net
-    cinst windirstat
-    cinst sysinternals
-    cinst evernote
-    cinst NugetPackageExplorer
-    cinst PowerGUI
-    cinst InputDirector
-    cinst QTTabBar
-    cinst Firefox
-    cinst windbg    
-
-    cinst  googlechrome
     Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
 
     Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
-    cinst resharper
-    cinst TestDriven.Net
 
-    cinst dotpeek
     $dotPeekDir = (Get-ChildItem $env:systemdrive\chocolatey\lib\dotpeek* | select $_.last)
     Install-ChocolateyFileAssociation".dll" "$dotPeekDir\tools\dotPeek.exe"
 
-    Install-ChocolateyEnvironmentVariable "devFolder" "d:\dev"
     Install-ChocolateyPath "C:\Chocolatey\chocolateyInstall" "Machine"
 
-    cinst autohotkey_l
     $ahk = "$env:appdata\Microsoft\Windows\Start Menu\Programs\startup\AutoScript.ahk"
     set-content $ahk -Force -value @"
 ^+C::
@@ -80,11 +53,6 @@ else
 }
 "@
 .$ahk
-
-mkdir d:\dev\wit\Q11W
-cd d:\dev\wit\Q11W
-$tf = "${env:ProgramFiles(x86)}\Microsoft Visual Studio 11.0\Common7\IDE\tf.exe"
-.$tf workspace /new /noprompt /template:wrockdesk-q11w_template $env:computername-q11w /collection:http://vstspioneer:8080/tfs/vstsdf /location:local
 } 
 catch {
     Write-ChocolateyFailure 'mwrock-TfsDesktop' $($_.Exception.Message)
