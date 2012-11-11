a0f702668ce20ffcf52cc69df0b3a4919594eb5a
Matt Wrock <matt@mattwrock.com>
2012-11-10 19:23:47 -0800

    fix office install

--- Office2013ProPlusPreview/tools/chocolateyInstall.ps1
+++ Office2013ProPlusPreview/tools/chocolateyInstall.ps1
@@ -1,4 +1,3 @@
 $configFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'config.xml')
-
-Install-ChocolateyPackage 'office2013propluspreview' 'exe' "/extract:office /log:log.txt /quiet /norestart" 'http://care.dlservice.microsoft.com//dl/download/4/7/1/4712B4E1-4DD9-4468-B8A4-507D7F988B1F/professionalplus_en-us_x86.exe?lcid=1033' 'http://care.dlservice.microsoft.com//dl/download/4/7/1/4712B4E1-4DD9-4468-B8A4-507D7F988B1F/professionalplus_en-us_x64.exe?lcid=1033'
-Install-ChocolateyInstallPackage 'office2013propluspreview' 'exe' "/config $configFile" 'office\setup.exe'
+Install-ChocolateyPackage 'office2013propluspreview' 'exe' "/extract:$env:temp\office /log:$env:temp\officeInstall.log /quiet /norestart" 'http://care.dlservice.microsoft.com//dl/download/4/7/1/4712B4E1-4DD9-4468-B8A4-507D7F988B1F/professionalplus_en-us_x86.exe?lcid=1033'
+Install-ChocolateyInstallPackage 'office2013propluspreview' 'exe' "/config $configFile" "$env:temp\office\setup.exe"
