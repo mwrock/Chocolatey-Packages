24c8a96920901628c603c639083efa706c685bbd
Matt Wrock <matt@mattwrock.com>
2012-12-31 11:31:14 -0800

    update firefox version

--- Firefox/Tools/ChocolateyInstall.ps1
+++ Firefox/Tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'firefox' 'exe' '-ms' 'http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/15.0/win32/en-US/Firefox%20Setup%2015.0.exe'
+Install-ChocolateyPackage 'firefox' 'exe' '-ms' 'http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.1/win32/en-US/Firefox%20Setup%2017.0.1.exe'
