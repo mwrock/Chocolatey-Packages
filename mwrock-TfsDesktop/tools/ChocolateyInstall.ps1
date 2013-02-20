1537a9b052001c0b818bd31b6f6724d044a01dea
Matt Wrock <matt@mattwrock.com>
2013-02-19 17:34:06 -0800

    updated work package

--- mwrock-TfsDesktop/tools/ChocolateyInstall.ps1
+++ mwrock-TfsDesktop/tools/ChocolateyInstall.ps1
@@ -1,58 +1,71 @@
-try {
-    import-module $env:systemdrive\tools\boxStarter\boxstarter.psm1
-    Move-LibraryDirectory "Personal" "$env:UserProfile\skydrive\documents"
-    Move-LibraryDirectory "Downloads" "D:\Downloads"
+Install-WindowsUpdate -AcceptEula
+Update-ExecutionPolicy Unrestricted
+Move-LibraryDirectory "Personal" "$env:UserProfile\skydrive\documents"
 
-    Install-WindowsUpdate
+#Stock TFS dev environment
+#& \\vscsstor\Tools\tfsbootstrapper\build full-install -sourcePath d:\dev\wit\src -binariesPath c:\dev\wit\binaries -branch $/devdiv/feature/q11w_mwrock2
+#& \\vscsstor\Tools\tfsbootstrapper\build create-workspace -sourcePath d:\dev\wit\q11w -binariesPath c:\dev\wit\q11w\binaries -branch $/devdiv/d11rel/q11w
 
-    #Stock TFS dev environment
-    #& \\vscsstor\Tools\tfsbootstrapper\build full-install -sourcePath d:\dev\wit\src -binariesPath c:\dev\wit\binaries -branch $/devdiv/feature/q11w_mwrock2
-    #& \\vscsstor\Tools\tfsbootstrapper\build create-workspace -sourcePath d:\dev\wit\q11w -binariesPath c:\dev\wit\q11w\binaries -branch $/devdiv/d11rel/q11w
-    
-    Install-ChocolateyPinnedTaskBarItem "$env:programfiles\console\console.exe"
-    copy-item (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'console.xml') -Force $env:appdata\console\console.xml
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+Set-TaskbarSmall
+Enable-RemoteDesktop
 
-    Set-TaskbarSmall
-    Enable-RemoteDesktop
-    cinst TelnetClient -source windowsFeatures
-    cinst Microsoft-Hyper-V -source windowsFeatures
-    cinst Microsoft-Hyper-V-Management-Clients -source windowsFeatures
-    Install-ChocolateyPinnedTaskBarItem "$env:windir\system32\mstsc.exe"
+cinstm git-credential-winstore 
+cinstm console-devel 
+cinstm sublimetext2 
+cinstm skydrive 
+cinstm fiddler 
+cinstm poshgit       
+cinstm winrar 
+cinstm Paint.net 
+cinstm windirstat 
+cinstm sysinternals 
+cinstm evernote 
+cinstm NugetPackageExplorer 
+cinstm InputDirector 
+cinstm QTTabBar 
+cinstm Firefox 
+cinstm windbg 
+cinstm googlechrome 
+cinstm resharper 
+cinstm TestDriven.Net 
+cinstm dotpeek 
+cinstm autohotkey_l 
 
-    $sublimeDir = (Get-ChildItem $env:systemdrive\chocolatey\lib\sublimetext* | select $_.last)
-    $sublimeExe = "$sublimeDir\tools\sublime_text.exe"
-    copy-item (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'sublime\*') -Force -Recurse "$sublimeDir\tools\data"
-    move-item (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) "$sublimeDir\tools\data\Pristine Packages") -Force "$sublimeDir\tools"    
-    Install-ChocolateyPinnedTaskBarItem $sublimeExe
-    Install-ChocolateyExplorerMenuItem "sublime" "Open with Sublime Text 2" $sublimeExe
-    Install-ChocolateyExplorerMenuItem "sublime" "Open with Sublime Text 2" $sublimeExe "directory"
-    Install-ChocolateyFileAssociation ".txt" $sublimeExe
-    Install-ChocolateyFileAssociation "." txtfile
+cinst Microsoft-Hyper-V-All -source windowsFeatures
+cinst TelnetClient -source windowsFeatures
 
-    Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
+Install-ChocolateyVsixPackage vscommands http://visualstudiogallery.msdn.microsoft.com/a83505c6-77b3-44a6-b53b-73d77cba84c8/file/74740/18/SquaredInfinity.VSCommands.VS11.vsix
 
-    Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
+$sublimeDir = "$env:programfiles\Sublime Text 2"
 
-    $dotPeekDir = (Get-ChildItem $env:systemdrive\chocolatey\lib\dotpeek* | select $_.last)
-    Install-ChocolateyFileAssociation".dll" "$dotPeekDir\tools\dotPeek.exe"
+Install-ChocolateyPinnedTaskBarItem "$env:windir\system32\mstsc.exe"
+Install-ChocolateyPinnedTaskBarItem "$env:programfiles\console\console.exe"
+Install-ChocolateyPinnedTaskBarItem "$sublimeDir\sublime_text.exe"
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Google\Chrome\Application\chrome.exe"
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
 
-    $ahk = "$env:appdata\Microsoft\Windows\Start Menu\Programs\startup\AutoScript.ahk"
-    set-content $ahk -Force -value @"
+#Install-ChocolateyFileAssociation "." txtfile
+Install-ChocolateyFileAssociation ".txt" "$env:programfiles\Sublime Text 2\sublime_text.exe"
+Install-ChocolateyFileAssociation ".dll" "$($Boxstarter.ChocolateyBin)\dotPeek.bat"
+
+mkdir "$sublimeDir\data"
+copy-item (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'sublime\*') -Force -Recurse "$sublimeDir\data"
+move-item "$sublimeDir\data\Pristine Packages\*" -Force "$sublimeDir\Pristine Packages"
+copy-item (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'console.xml') -Force $env:appdata\console\console.xml
+
+$ahk = "$env:appdata\Microsoft\Windows\Start Menu\Programs\startup\AutoScript.ahk"
+set-content $ahk -Force -value @"
 ^+C::
 IfWinExist Console
 {
-    WinActivate
+WinActivate
 }
 else
 {
-    Run Console
-    WinWait Console
-    WinActivate
+Run Console
+WinWait Console
+WinActivate
 }
 "@
 .$ahk
-} 
-catch {
-    Write-ChocolateyFailure 'mwrock-TfsDesktop' $($_.Exception.Message)
-    throw 
-}
