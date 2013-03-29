359a313b47e6c1344dc7e258ea74a732c1805381
Matt Wrock <matt@mattwrock.com>
2013-03-29 00:50:19 -0700

    added office home premium

--- Office2013ProPlusPreview/tools/chocolateyInstall.ps1
+++
@@ -1,3 +0,0 @@
-$configFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'config.xml')
-Install-ChocolateyPackage 'office2013propluspreview' 'exe' "/extract:$env:temp\office /log:$env:temp\officeInstall.log /quiet /norestart" 'http://care.dlservice.microsoft.com//dl/download/4/7/1/4712B4E1-4DD9-4468-B8A4-507D7F988B1F/professionalplus_en-us_x86.exe?lcid=1033'
-Install-ChocolateyInstallPackage 'office2013propluspreview' 'exe' "/config $configFile" "$env:temp\office\setup.exe"
