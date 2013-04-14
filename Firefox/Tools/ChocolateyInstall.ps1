ceabf0767db7d3bf75dd12ec3bee73dd2fa4128c
Matt Wrock <matt@mattwrock.com>
2013-04-13 22:23:16 -0700

    updated firefox to 20.0.1

--- Firefox/Tools/ChocolateyInstall.ps1
+++ Firefox/Tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'firefox' 'exe' '-ms' 'http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/19.0.2/win32/en-US/Firefox%20Setup%2019.0.2.exe'
+Install-ChocolateyPackage 'firefox' 'exe' '-ms' 'http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/20.0.1/win32/en-US/Firefox%20Setup%2020.0.1.exe'
