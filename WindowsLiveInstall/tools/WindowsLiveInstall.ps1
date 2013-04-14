51366e70e55094a79d2992c67ce1bfad6cdccad4
Matt Wrock <matt@mattwrock.com>
2013-04-13 23:14:21 -0700

    upgrading windows live writer to 2012 version

--- WindowsLiveInstall/tools/WindowsLiveInstall.ps1
+++ WindowsLiveInstall/tools/WindowsLiveInstall.ps1
@@ -7,7 +7,7 @@ try {
     $tempDir = Join-Path $env:TEMP "chocolatey\WindowsLive-mw"
     if (!(Test-Path $tempDir)) {New-Item $tempDir -type Directory}
     $file = Join-Path $tempDir "wlsetup-all.exe"
-    if(!(Test-Path $file)) { Get-ChocolateyWebFile WindowsLive $file http://g.live.com/1rewlive4-all/en/wlsetup-all.exe }
+    if(!(Test-Path $file)) { Get-ChocolateyWebFile WindowsLive $file http://g.live.com/1rewlive5-all/en/wlsetup-all.exe }
     Install-ChocolateyInstallPackage "WindowsLiveMesh" 'exe' "/q /NOToolbarCEIP /NOhomepage /Nolaunch /nosearch /AppSelect:$product" $file
     Write-ChocolateySuccess $package
 } catch {
