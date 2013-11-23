68e7ab0563f82a3b7c9360f0827e0fa2e4a4b4f5
Matt Wrock <matt@mattwrock.com>
2013-11-22 22:33:45 -0800

    updated inputdirector to build 101

--- InputDirector/tools/ChocolateyInstall.ps1
+++ InputDirector/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 try {
     $drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
     Install-ChocolateyZipPackage 'InputDirector' 'http://www.inputdirector.com/downloads/InputDirector.v1.3.zip' $drop
-    ."$drop/InputDirector.v1.3.build100.Setup.exe" /S
+    ."$drop/InputDirector.v1.3.build101.Setup.exe" /S
     Write-ChocolateySuccess 'InputDirector'
 } catch {
     Write-ChocolateyFailure 'InputDirector' $($_.Exception.Message)
