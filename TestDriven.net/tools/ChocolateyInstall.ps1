c97e5244a850a00310aaf5c033533cdfd3170edf
Matt Wrock <matt@mattwrock.com>
2014-01-09 21:20:45 -0800

    updating testdriven.net

--- TestDriven.net/tools/ChocolateyInstall.ps1
+++ TestDriven.net/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 try {
     $drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
     $exe = "$drop\Setup.exe"
-    Install-ChocolateyZipPackage 'TestDriven' 'http://www.testdriven.net/downloads/TestDriven.NET-3.3.2779_Personal_Beta2.zip' $drop
+    Install-ChocolateyZipPackage 'TestDriven' 'http://www.testdriven.net/downloads/TestDriven.NET-3.5.2830_Personal_RTM.zip' $drop
     Install-ChocolateyInstallPackage "TestDriven" 'exe' "/quiet" $exe
 
     Write-ChocolateySuccess 'TestDriven.Net'
