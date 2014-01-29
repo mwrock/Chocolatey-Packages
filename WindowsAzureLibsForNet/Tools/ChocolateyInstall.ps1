63277cd07a5f8deec3a1b11ffed4e3fd84d79696
Matt Wrock <matt@mattwrock.com>
2014-01-28 22:54:24 -0800

    check if azure packages are already installed

--- WindowsAzureLibsForNet/Tools/ChocolateyInstall.ps1
+++ WindowsAzureLibsForNet/Tools/ChocolateyInstall.ps1
@@ -1,1 +1,5 @@
+if(Test-Path "$env:ProgramFiles\Microsoft SDKs\Windows Azure\.NET SDK\v2.2") {
+    Write-Host "Windows Azure Libraries for .net v2.2 is already installed."
+    return
+}
 Install-ChocolateyPackage 'WindowsAzureLibsForNet' 'msi' '/quiet /norestart' 'http://download.microsoft.com/download/2/8/8/2886D7B8-3C4E-4372-8F86-474E06526299/WindowsAzureLibsForNet-x86.msi' 'http://download.microsoft.com/download/2/8/8/2886D7B8-3C4E-4372-8F86-474E06526299/WindowsAzureLibsForNet-x64.msi'
--- WindowsAzurePowershell/Tools/ChocolateyInstall.ps1
+++ WindowsAzurePowershell/Tools/ChocolateyInstall.ps1
@@ -1,1 +1,11 @@
+if(${env:ProgramFiles(x86)} -ne $null){ $programFiles86 = ${env:ProgramFiles(x86)} } else { $programFiles86 = $env:ProgramFiles }
+$modulePath="$programFiles86\Microsoft SDKs\Windows Azure\PowerShell\Azure\Azure.psd1"
+if(Test-Path $modulePath) {
+    $a=IEX (Get-content $modulePath | Out-String)
+    if($a.ModuleVersion -eq "0.7.2") {
+        Write-Host "Windows Azure Powershell is already installed."
+        return
+    }
+}
+
 Install-ChocolateyPackage 'WindowsAzurePowershell' 'msi' '/quiet /norestart' 'http://az412849.vo.msecnd.net/downloads02/windowsazure-powershell.0.7.2.1.msi'
