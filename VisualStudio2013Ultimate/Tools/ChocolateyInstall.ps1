e70bd946351bc78a205c783257ad47cfbd84ded6
Ken Sykora <ksykora@nerdery.com>
2014-04-05 17:11:22 -0500

    Updated Ultimate script

--- VisualStudio2013Ultimate/Tools/ChocolateyInstall.ps1
+++ VisualStudio2013Ultimate/Tools/ChocolateyInstall.ps1
@@ -1,16 +1,45 @@
 $adminFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'AdminDeployment.xml')
 $customArgs = $env:chocolateyInstallArguments
 $env:chocolateyInstallArguments=""
-if($customArgs.Length -gt 0){
-    $featuresToAdd = -split $customArgs
-    [xml]$adminXml=Get-Content $adminFile
-    $featuresToAdd | % {
-        $feature=$_
-        $node=$adminXml.DocumentElement.SelectableItemCustomizations.ChildNodes | ? {$_.Id -eq "$feature"}
-        if($node -ne $null){
-            $node.Selected="yes"
-        }
+
+$MATCH_PATTERN = "/([a-zA-Z]+):([`"'])?([a-zA-Z- 0-9_]+)([`"'])?"
+$PARAMATER_NAME_INDEX = 1
+$VALUE_INDEX = 3
+
+$productKey = ""
+
+if($customArgs -match $MATCH_PATTERN ){
+    
+    write-debug "=====matches====="
+    $customArgValues = @{ }
+    $customArgs | Select-String $MATCH_PATTERN -AllMatches  | % matches | % { 
+        $customArgValues.Add(
+            $_.Groups[$PARAMATER_NAME_INDEX].Value.Trim(),
+            $_.Groups[$VALUE_INDEX].Value.Trim()) 
     }
-    $adminXml.Save($adminFile)
+
+    if($customArgValues['Features']) {
+        $featuresToAdd = -split $customArgValues['Features']
+        [xml]$adminXml=Get-Content $adminFile
+
+        $featuresToAdd | % {
+            $feature=$_
+            $node=$adminXml.DocumentElement.SelectableItemCustomizations.ChildNodes | ? {$_.Id -eq "$feature"}
+            if($node -ne $null){
+                $node.Selected="yes"
+            }
+        }
+        $adminXml.Save($adminFile)
+    }
+
+    if($customArgValues['ProductKey']) {
+        $productKey = $customArgValues['ProductKey']
+    };
 }
-Install-ChocolateyPackage 'VisualStudio2013Ultimate' 'exe' "/Passive /NoRestart /AdminFile $adminFile /Log $env:temp\vs.log" 'http://download.microsoft.com/download/C/F/B/CFBB5FF1-0B27-42E0-8141-E4D6DA0B8B13/vs_ultimate_download.exe'
+
+$installerArgs = "/Passive /NoRestart /AdminFile $adminFile /Log $env:temp\vs.log"
+if($productKey -ne "") {
+    $installerArgs = "${installerArgs} /ProductKey ${productKey}"
+}
+
+Install-ChocolateyPackage 'VisualStudio2013Ultimate' 'exe' $installerArgs 'http://download.microsoft.com/download/C/F/B/CFBB5FF1-0B27-42E0-8141-E4D6DA0B8B13/vs_ultimate_download.exe'
