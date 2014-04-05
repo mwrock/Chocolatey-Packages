80e960034fb47fd079cfea51c04bb8358fe2e4cd
Ken Sykora <ksykora@nerdery.com>
2014-04-05 17:57:39 -0500

    Refactored for premium

--- VisualStudio2013Premium/Tools/ChocolateyInstall.ps1
+++ VisualStudio2013Premium/Tools/ChocolateyInstall.ps1
@@ -1,46 +1,12 @@
+. (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'common.ps1')
+
 $adminFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'AdminDeployment.xml')
 $customArgs = $env:chocolateyInstallArguments
 $env:chocolateyInstallArguments=""
 
-$MATCH_PATTERN = "/([a-zA-Z]+):([`"'])?([a-zA-Z- 0-9_]+)([`"'])?"
-$PARAMATER_NAME_INDEX = 1
-$VALUE_INDEX = 3
+$settings = Initialize-VS-Settings $customArgs
 
-$productKey = ""
-
-if($customArgs -match $MATCH_PATTERN ){
-    
-    write-debug "=====matches====="
-    $customArgValues = @{ }
-    $customArgs | Select-String $MATCH_PATTERN -AllMatches  | % matches | % { 
-        $customArgValues.Add(
-            $_.Groups[$PARAMATER_NAME_INDEX].Value.Trim(),
-            $_.Groups[$VALUE_INDEX].Value.Trim()) 
-    }
-
-    if($customArgValues['Features']) {
-        $featuresToAdd = -split $customArgValues['Features']
-        [xml]$adminXml=Get-Content $adminFile
-
-        $featuresToAdd | % {
-            $feature=$_
-            $node=$adminXml.DocumentElement.SelectableItemCustomizations.ChildNodes | ? {$_.Id -eq "$feature"}
-            if($node -ne $null){
-                $node.Selected="yes"
-            }
-        }
-        $adminXml.Save($adminFile)
-    }
-
-    if($customArgValues['ProductKey']) {
-        $productKey = $customArgValues['ProductKey']
-    };
-}
-
-$installerArgs = "/Passive /NoRestart /AdminFile $adminFile /Log $env:temp\vs.log"
-if($productKey -ne "") {
-    $installerArgs = "${installerArgs} /ProductKey ${productKey}"
-}
+$installerArgs = Get-VS-Installer-Args $settings.ProductKey
 
 Install-ChocolateyPackage 'VisualStudio2013Premium' 'exe' $installerArgs 'http://download.microsoft.com/download/D/B/D/DBDEE6BB-AF28-4C76-A5F8-710F610615F7/vs_premium_download.exe'
  
