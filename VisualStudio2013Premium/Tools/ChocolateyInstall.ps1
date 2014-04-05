8009e90cead33854ce60c6841cada06874fcf81b
Ken Sykora <ksykora@nerdery.com>
2014-04-05 17:05:01 -0500

    Updated script for 2013 Premium

--- VisualStudio2013Premium/Tools/ChocolateyInstall.ps1
+++ VisualStudio2013Premium/Tools/ChocolateyInstall.ps1
@@ -1,17 +1,46 @@
 $adminFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'AdminDeployment.xml')
 $customArgs = $env:chocolateyInstallArguments
 $env:chocolateyInstallArguments=""
-if($customArgs.Length -gt 0){
-    $featuresToAdd = -split $customArgs
-    [xml]$adminXml=Get-Content $adminFile
-    $featuresToAdd | % {
-        $feature=$_
-        $node=$adminXml.DocumentElement.SelectableItemCustomizations.ChildNodes | ? {$_.Id -eq "$feature"}
-        if($node -ne $null){
-            $node.Selected="yes"
-        }
+
+$MATCH_PATTERN = "/([a-zA-Z]+):([`"'])?([a-zA-Z- 0-9_]+)([`"'])?"
+$PARAMATER_NAME_INDEX = 1
+$VALUE_INDEX = 3
+
+$productKey = ""
+
+if($customArgs -match $MATCH_PATTERN ){
+    
+    write-debug "=====matches====="
+    $customArgValues = @{ }
+    $customArgs | Select-String $MATCH_PATTERN -AllMatches  | % matches | % { 
+        $customArgValues.Add(
+            $_.Groups[$PARAMATER_NAME_INDEX].Value,
+            $_.Groups[$VALUE_INDEX].Value) 
     }
-    $adminXml.Save($adminFile)
+
+    if($customArgValues['Features']) {
+        $featuresToAdd = -split $customArgValues['Features']
+        [xml]$adminXml=Get-Content $adminFile
+
+        $featuresToAdd | % {
+            $feature=$_
+            $node=$adminXml.DocumentElement.SelectableItemCustomizations.ChildNodes | ? {$_.Id -eq "$feature"}
+            if($node -ne $null){
+                $node.Selected="yes"
+            }
+        }
+        $adminXml.Save($adminFile)
+    }
+
+    if($customArgValues['ProductKey']) {
+        $productKey = $customArgValues['ProductKey']
+    };
 }
-Install-ChocolateyPackage 'VisualStudio2013Premium' 'exe' "/Passive /NoRestart /AdminFile $adminFile /Log  $env:temp\vs.log" 'http://download.microsoft.com/download/D/B/D/DBDEE6BB-AF28-4C76-A5F8-710F610615F7/vs_premium_download.exe'
+
+$installerArgs = "/Passive /NoRestart /AdminFile $adminFile /Log $env:temp\vs.log"
+if($productKey -ne "") {
+    $installerArgs = "${installerArgs} /ProductKey ${productKey}"
+}
+
+Install-ChocolateyPackage 'VisualStudio2013Premium' 'exe' $installerArgs 'http://download.microsoft.com/download/D/B/D/DBDEE6BB-AF28-4C76-A5F8-710F610615F7/vs_premium_download.exe'
  
