343a41dd58afcf9867c338ec8cb2bada9272d0d4
Ken Sykora <ksykora@nerdery.com>
2014-04-05 16:01:04 -0500

    Refactored to include delimited param set

--- VisualStudio2013Professional/Tools/ChocolateyInstall.ps1
+++ VisualStudio2013Professional/Tools/ChocolateyInstall.ps1
@@ -2,31 +2,31 @@ $adminFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $customArgs = $env:chocolateyInstallArguments
 $env:chocolateyInstallArguments=""
 $productKey = ""
+
+$ARGUMENTS_DELIMITER = ';'
+$FEATURES_INDEX = 0
+$PRODUCT_KEY_INDEX = 1
+
 if($customArgs.Length -gt 0){
-    $featuresToAdd = -split $customArgs
+    $featuresToAdd = ($customArgs -split $ARGUMENTS_DELIMITER)[$FEATURES_INDEX]
+    $productKey = ($customArgs -split $ARGUMENTS_DELIMITER)[$PRODUCT_KEY_INDEX]
+
+    $featuresToAdd = -split $featuresToAdd
     [xml]$adminXml=Get-Content $adminFile
 
     $featuresToAdd | % {
         $feature=$_
-        if($feature.Contains("-")) { #Product Key's contain hyphens, features do not
-            $productKey = $feature
-        } else {
-            $node=$adminXml.DocumentElement.SelectableItemCustomizations.ChildNodes | ? {$_.Id -eq "$feature"}
-            if($node -ne $null){
-                $node.Selected="yes"
-            }
+        $node=$adminXml.DocumentElement.SelectableItemCustomizations.ChildNodes | ? {$_.Id -eq "$feature"}
+        if($node -ne $null){
+            $node.Selected="yes"
         }
     }
     $adminXml.Save($adminFile)
 }
 
-Write-Debug "Product Key: ${productKey}"
-
 $installerArgs = "/Passive /NoRestart /AdminFile $adminFile /Log $env:temp\vs.log"
 if($productKey -ne "") {
     $installerArgs = "${installerArgs} /ProductKey ${productKey}"
 }
-
-Write-Debug "Args: ${installerArgs}"
 
 Install-ChocolateyPackage 'VisualStudio2013Professional' 'exe' $installerArgs 'http://download.microsoft.com/download/A/F/1/AF128362-A6A8-4DB3-A39A-C348086472CC/vs_professional_download.exe'
