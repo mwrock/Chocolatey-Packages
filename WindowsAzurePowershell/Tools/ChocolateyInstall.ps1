a8ec31864495230266fc88b2ad14db5b8c9cb754
Matt Wrock <matt@mattwrock.com>
2014-04-05 23:37:56 -0700

    updating windows azure powershell to 0.7.4 and showing sensible message if ps3 is not installed

--- WindowsAzurePowershell/Tools/ChocolateyInstall.ps1
+++ WindowsAzurePowershell/Tools/ChocolateyInstall.ps1
@@ -1,11 +1,16 @@
+if(!(Test-Path "HKLM:\Software\Microsoft\PowerShell\3")){
+    Write-Host "Windows Azure Powershell requires Powershell version 3 or greater."
+    return
+}
+
 if(${env:ProgramFiles(x86)} -ne $null){ $programFiles86 = ${env:ProgramFiles(x86)} } else { $programFiles86 = $env:ProgramFiles }
 $modulePath="$programFiles86\Microsoft SDKs\Windows Azure\PowerShell\Azure\Azure.psd1"
 if(Test-Path $modulePath) {
     $a=IEX (Get-content $modulePath | Out-String)
-    if($a.ModuleVersion -eq "0.7.2") {
+    if($a.ModuleVersion -eq "0.7.4") {
         Write-Host "Windows Azure Powershell is already installed."
         return
     }
 }
 
-Install-ChocolateyPackage 'WindowsAzurePowershell' 'msi' '/quiet /norestart' 'http://az412849.vo.msecnd.net/downloads02/windowsazure-powershell.0.7.2.1.msi'
+Install-ChocolateyPackage 'WindowsAzurePowershell' 'msi' '/quiet /norestart' 'http://az412849.vo.msecnd.net/downloads02/windowsazure-powershell.0.7.4.msi'
