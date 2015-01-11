268ea1ea87d8b487c940b301509d69471ee7c9ad
Matt Wrock <matt@mattwrock.com>
2015-01-10 23:12:32 -0800

    updating WindowsAzureLibsForNet to v2.5

--- WindowsAzureLibsForNet/Tools/ChocolateyInstall.ps1
+++ WindowsAzureLibsForNet/Tools/ChocolateyInstall.ps1
@@ -1,5 +1,5 @@
-if(Test-Path "$env:ProgramFiles\Microsoft SDKs\Windows Azure\.NET SDK\v2.2") {
-    Write-Host "Windows Azure Libraries for .net v2.2 is already installed."
+if(Test-Path "$env:ProgramFiles\Microsoft SDKs\Azure\.NET SDK\v2.5") {
+    Write-Host "Windows Azure Libraries for .net v2.5 is already installed."
     return
 }
-Install-ChocolateyPackage 'WindowsAzureLibsForNet' 'msi' '/quiet /norestart' 'http://download.microsoft.com/download/2/8/8/2886D7B8-3C4E-4372-8F86-474E06526299/WindowsAzureLibsForNet-x86.msi' 'http://download.microsoft.com/download/2/8/8/2886D7B8-3C4E-4372-8F86-474E06526299/WindowsAzureLibsForNet-x64.msi'
+Install-ChocolateyPackage 'WindowsAzureLibsForNet' 'msi' '/quiet /norestart' 'http://download.microsoft.com/download/9/F/7/9F7D3299-9AE1-40BE-B24F-C0E9EB0BE61E/MicrosoftAzureLibsForNet-x86.msi' 'http://download.microsoft.com/download/9/F/7/9F7D3299-9AE1-40BE-B24F-C0E9EB0BE61E/MicrosoftAzureLibsForNet-x64.msi'
