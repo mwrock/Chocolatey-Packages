c8889a35b89e349c51d3f289b796292a9116306c
Matt Wrock <matt@mattwrock.com>
2015-08-23 18:56:23 -0700

    new choco no longer includes version in path so anticipate it might not be there

--- WindowsLiveWriter/tools/ChocolateyInstall.ps1
+++ WindowsLiveWriter/tools/ChocolateyInstall.ps1
@@ -1,2 +1,2 @@
-$installScriptDir = ([array](dir $env:ChocolateyInstall\lib\WindowsLiveInstaller.*))[-1]
+$installScriptDir = ([array](dir $env:ChocolateyInstall\lib\WindowsLiveInstaller*))[-1]
 .$installScriptDir\tools\WindowsLiveInstall.ps1 "Windows Live Writer" "writer"
