9d98189549276b2e5dd17efb690643cf19ceeb03
Matt Wrock <matt@mattwrock.com>
2015-11-05 06:18:13 -0800

    deprecating git-credential-winstore

--- Git-Credential-WinStore/tools/ChocolateyInstall.ps1
+++ Git-Credential-WinStore/tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'git-credential-winstore' 'exe' '-s' 'https://github.com/downloads/anurse/git-credential-winstore/git-credential-winstore.exe'
+write-error "The git-credential-winstore is no longer maintained. Use the Git Credential Manager for Windows instead. The Git Credential Manager for Windows can be installed using 'choco install gcm -pre'."
