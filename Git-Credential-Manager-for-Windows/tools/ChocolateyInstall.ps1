e0588cafb689e6e0e2cab0731ec2a4e7a121a166
Matt Wrock <matt@mattwrock.com>
2015-12-05 22:54:40 -0800

    adding Git-Credential-Manager-for-Windows

---
+++ Git-Credential-Manager-for-Windows/tools/ChocolateyInstall.ps1
@@ -0,0 +1,7 @@
+# Run installer
+$packageName = 'Git-Credential-Manager-for-Windows'
+$url = 'https://github.com/Microsoft/Git-Credential-Manager-for-Windows/releases/download/v1.0.0/Setup.exe'
+$installerType = 'exe'
+$silentArgs = '/SILENT /NORESTART'
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url
