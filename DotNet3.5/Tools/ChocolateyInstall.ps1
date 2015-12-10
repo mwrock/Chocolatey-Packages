371e38df4142be228bf122421ddd9ae1432f0fa0
Matt Wrock <matt@mattwrock.com>
2015-12-09 21:40:20 -0800

    update .net3.5 package to use dism directly instead of using windowsfeatures feed

--- DotNet3.5/Tools/ChocolateyInstall.ps1
+++ DotNet3.5/Tools/ChocolateyInstall.ps1
@@ -1,5 +1,7 @@
 if(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
-    cinst NetFx3 -source windowsfeatures
+  $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3 /All"
+  $statements = "cmd.exe $packageArgs"
+  Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0)
 }
 else {
      Write-Host "Microsoft .Net 3.5 Framework is already installed on your machine."
