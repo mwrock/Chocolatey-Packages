aebdf4db04720aa9e16276a8aec0fc1fa14c38bd
Matt Wrock <matt@mattwrock.com>
2015-12-15 16:15:17 -0800

    fixing windowslivewriter to depend on dot net 3.5 and handle 3010

--- WindowsLiveInstall/tools/WindowsLiveInstall.ps1
+++ WindowsLiveInstall/tools/WindowsLiveInstall.ps1
@@ -8,7 +8,7 @@ try {
     if (!(Test-Path $tempDir)) {New-Item $tempDir -type Directory}
     $file = Join-Path $tempDir "wlsetup-all.exe"
     if(!(Test-Path $file)) { Get-ChocolateyWebFile WindowsLive $file http://g.live.com/1rewlive5-all/en/wlsetup-all.exe }
-    Install-ChocolateyInstallPackage "WindowsLiveMesh" 'exe' "/q /NOToolbarCEIP /NOhomepage /Nolaunch /nosearch /AppSelect:$product" $file
+    Install-ChocolateyInstallPackage $product 'exe' "/q /NOToolbarCEIP /NOhomepage /Nolaunch /nosearch /AppSelect:$product" $file @(0, 3010)
     Write-ChocolateySuccess $package
 } catch {
     Write-ChocolateyFailure $package $($_.Exception.Message)
