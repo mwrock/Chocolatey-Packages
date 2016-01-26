164557607b55f827e2b67540c6b9dec37d90cfd2
Matt Wrock <matt@mattwrock.com>
2016-01-26 00:04:04 -0800

    fix moderation issues with vs 2012 packages

--- VisualStudio2012Premium/Tools/ChocolateyInstall.ps1
+++ VisualStudio2012Premium/Tools/ChocolateyInstall.ps1
@@ -1,8 +1,7 @@
 . (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'common.ps1')
 
 $adminFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'AdminDeployment.xml')
-$customArgs = $env:chocolateyInstallArguments
-$env:chocolateyInstallArguments=""
+$customArgs = $env:chocolateyPackageParameters
 
 $settings = Initialize-VS-Settings $customArgs $adminFile
 $installerArgs = Get-VS-Installer-Args $settings.ProductKey
