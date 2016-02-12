93126cad8e7d44b27218f1248fc4751eb09f1f25
Matt Wrock <matt@mattwrock.com>
2016-02-12 05:51:04 -0800

    fixing dism command in win7/2k8r2

--- DotNet3.5/Tools/ChocolateyInstall.ps1
+++ DotNet3.5/Tools/ChocolateyInstall.ps1
@@ -1,5 +1,9 @@
 if(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
-  $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3 /All"
+  $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3ServerFeatures"
+  $statements = "cmd.exe $packageArgs"
+  Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0)
+
+  $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3"
   $statements = "cmd.exe $packageArgs"
   Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0)
 }
