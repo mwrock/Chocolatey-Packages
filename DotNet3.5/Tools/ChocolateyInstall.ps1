9c3307d59efbcd49adb94624a91992a21b3de182
Matt Wrock <matt@mattwrock.com>
2016-02-19 22:55:48 -0800

    fixing dotnet3.5 package

--- DotNet3.5/Tools/ChocolateyInstall.ps1
+++ DotNet3.5/Tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 if(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
   $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3ServerFeatures"
   $statements = "cmd.exe $packageArgs"
-  Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0)
+  Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0, 1)
 
   $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3"
   $statements = "cmd.exe $packageArgs"
