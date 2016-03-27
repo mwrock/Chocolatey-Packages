1ec94644aed1a196948017337e805b129675d2ba
Matt Wrock <matt@mattwrock.com>
2016-03-27 06:04:41 -0700

    update git credential manager

--- Git-Credential-Manager-for-Windows/tools/ChocolateyInstall.ps1
+++ Git-Credential-Manager-for-Windows/tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 # Run installer
 $packageName = 'Git-Credential-Manager-for-Windows'
-$url = 'https://github.com/Microsoft/Git-Credential-Manager-for-Windows/releases/download/v1.1.0/GCMW-1.1.0.exe'
+$url = 'https://github.com/Microsoft/Git-Credential-Manager-for-Windows/releases/download/v1.2.2/GCMW-1.2.2.exe'
 $installerType = 'exe'
 $silentArgs = '/SILENT /NORESTART'
 
