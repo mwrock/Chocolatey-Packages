15544505e5852721b04dc57c443dc68e4641370a
Matt Wrock <matt@mattwrock.com>
2016-07-17 00:41:03 -0700

    only install NetFx3ServerFeatures on server skus

--- DotNet3.5/Tools/ChocolateyInstall.ps1
+++ DotNet3.5/Tools/ChocolateyInstall.ps1
@@ -1,8 +1,9 @@
 if(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
-  $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3ServerFeatures"
-  $statements = "cmd.exe $packageArgs"
-  Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0, 1)
-
+  if((wmic os get caption | Out-String).Contains("Server")) {
+      $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3ServerFeatures"
+      $statements = "cmd.exe $packageArgs"
+      Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0, 1)
+  }
   $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3"
   $statements = "cmd.exe $packageArgs"
   Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0)
