f0aaa6d35951972a9396da4edb9dad999e212034
Matt Wrock <matt@mattwrock.com>
2016-08-23 21:40:09 -0700

    add chefdk

---
+++ chefdk/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿try {
+  $name = 'chefdk'
+  $version = '0.17.17'
+  $url = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
+
+  $url64 = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
+  $installerType = 'MSI'
+  $silentArgs = "/qn /quiet /norestart"
+  $validExitCodes = @(0,3010)
+
+  $MD5Checksum = ''
+  Install-ChocolateyPackage "$name" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes -checksum $MD5Checksum -checksumtype 'md5'
+} catch {
+  Write-ChocolateyFailure $name $($_.Exception.Message)
+  throw
+}
