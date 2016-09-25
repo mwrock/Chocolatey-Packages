029bdb9194f637e4b94e634c6b9a2f70b7161f32
Matt Wrock <matt@mattwrock.com>
2016-09-25 04:04:27 -0700

    update input director

--- InputDirector/tools/ChocolateyInstall.ps1
+++ InputDirector/tools/ChocolateyInstall.ps1
@@ -1,9 +1,3 @@
-try {
-    $drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-    Install-ChocolateyZipPackage 'InputDirector' 'http://www.inputdirector.com/downloads/InputDirector.v1.3.zip' $drop
-    ."$drop/InputDirector.v1.3.build101.Setup.exe" /S
-    Write-ChocolateySuccess 'InputDirector'
-} catch {
-    Write-ChocolateyFailure 'InputDirector' $($_.Exception.Message)
-    throw 
-}
+$drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'InputDirector' 'https://www.inputdirector.com/downloads/InputDirector.v1.4.zip' $drop
+."$drop/InputDirector.v1.4.build110.Setup.exe" /S
