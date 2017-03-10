da88a9780bc22e8ba2ebbaf2a0a2499885a1655a
Steven Murawski <steven.murawski@gmail.com>
2017-03-10 10:55:35 -0600

    Update ChefDK to 1.2.22
    Signed-off-by: Steven Murawski <steven.murawski@gmail.com>

--- chefdk/tools/chocolateyinstall.ps1
+++ chefdk/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿try {
-  $name = 'chefdk'
-  $version = '1.1.16'
-  $url = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
+    $name = 'chefdk'
+    $version = '1.2.22'
+    $url = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
 
-  $url64 = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
-  $installerType = 'MSI'
-  $silentArgs = "/qn /quiet /norestart"
-  $validExitCodes = @(0,3010)
+    $url64 = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
+    $installerType = 'MSI'
+    $silentArgs = "/qn /quiet /norestart"
+    $validExitCodes = @(0,3010)
 
-  $MD5Checksum = ''
-  Install-ChocolateyPackage "$name" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes -checksum $MD5Checksum -checksumtype 'md5'
+    $MD5Checksum = ''
+    Install-ChocolateyPackage "$name" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes -checksum $MD5Checksum -checksumtype 'md5'
 } catch {
-  Write-ChocolateyFailure $name $($_.Exception.Message)
-  throw
+    Write-ChocolateyFailure $name $($_.Exception.Message)
+throw
 }
