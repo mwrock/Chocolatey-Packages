97823a7bdafd15b42f61aa2a64fcccb4f5dd11c4
Matt Wrock <matt@mattwrock.com>
2017-04-10 09:19:52 -0700

    bump chefdk to 1.3.40
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- chefdk/tools/chocolateyinstall.ps1
+++ chefdk/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿try {
     $name = 'chefdk'
-    $version = '1.2.22'
+    $version = '1.3.40'
     $url = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
 
     $url64 = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
