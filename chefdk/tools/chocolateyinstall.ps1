1ce25c47965e93c4172a7225d00d10646283a4d3
Joel Handwell <joelhandwell@gmail.com>
2017-07-20 22:57:59 -0400

    Update ChefDK Chocolatey package to 2.0.28

--- chefdk/tools/chocolateyinstall.ps1
+++ chefdk/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿try {
     $name = 'chefdk'
-    $version = '2.0.26'
+    $version = '2.0.28'
     $url = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
 
     $url64 = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
@@ -8,7 +8,7 @@
     $silentArgs = "/qn /quiet /norestart"
     $validExitCodes = @(0,3010)
 
-    $SHA256Checksum = 'd72cf4f86efc4b39be9c4e2ea677a414ccf7900b8f5a521711e706ed1737f31a'
+    $SHA256Checksum = '6967d43b14a358a5ee743f514b63d7e78cc9ca0789ce982577b31a759aecd3d0'
     Install-ChocolateyPackage "$name" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes -checksum $SHA256Checksum -checksumtype 'sha256'
 } catch {
     Write-ChocolateyFailure $name $($_.Exception.Message)
