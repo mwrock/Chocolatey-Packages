1efa78e74692d9e2ea526632264922f79412f5cd
mwrock <matt@mattwrock.com>
2018-01-10 10:48:58 -0800

    bump chefdk
    Signed-off-by: mwrock <matt@mattwrock.com>

--- chefdk/tools/chocolateyinstall.ps1
+++ chefdk/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿try {
     $name = 'chefdk'
-    $version = '2.3.4'
+    $version = '2.4.17'
     $url = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
     $url64 = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x64.msi"
     $installerType = 'MSI'
     $silentArgs = "/qn /quiet /norestart"
     $validExitCodes = @(0,3010)
 
-    $SHA256Checksum = 'f8326529c16b7ade644dcaff76a74d425cd9c54f95c7ba4128d3525d836deb26'
-    $SHA256Checksum64 = '7d784a45298e5c1e4ff206b73ba8544dff4e3218d7c24d5fb3cb0960fcc34322'
+    $SHA256Checksum = 'f6c4676461dc467ce6418a1466f697304054c764dc7208ad519aa4d27c75891b'
+    $SHA256Checksum64 = 'd46f1636089bb3883245db44256e179ff353c998f7f280db222daf504a78033f'
     Install-ChocolateyPackage "$name" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes -checksum $SHA256Checksum -checksumtype 'sha256' -checksum64 $SHA256Checksum64 -checksumtype64 'sha256'
 } catch {
     Write-ChocolateyFailure $name $($_.Exception.Message)
