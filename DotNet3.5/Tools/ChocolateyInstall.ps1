d18668990661dd66b8796dc7ee80b18aabc6cc84
girardc79 <girardc79@gmail.com>
2018-01-26 16:20:48 -0500

    Switch to PowerShell utilities for installing .NET on Windows Server.  This method allows for performing the installation remotely via WinRM.

--- DotNet3.5/Tools/ChocolateyInstall.ps1
+++ DotNet3.5/Tools/ChocolateyInstall.ps1
@@ -1,13 +1,19 @@
 if(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
-  if((wmic os get caption | Out-String).Contains("Server")) {
-      $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3ServerFeatures"
-      $statements = "cmd.exe $packageArgs"
-      Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0, 1)
+  $osString = wmic os get caption | Out-String
+  if($osString.Contains("Server")) {
+    if($osString.Contains("2008")) {
+      Add-WindowsFeature -Name NET-Framework-Core
+	}
+	else {
+	  Install-WindowsFeature -Name NET-Framework-Core
+	}
   }
-  $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3"
-  $statements = "cmd.exe $packageArgs"
-  Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0)
+  else {
+    $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3"
+    $statements = "cmd.exe $packageArgs"
+    Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0)
+  }
 }
 else {
-     Write-Host "Microsoft .Net 3.5 Framework is already installed on your machine."
- } 
+  Write-Host "Microsoft .Net 3.5 Framework is already installed on your machine."
+}
