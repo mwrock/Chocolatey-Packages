46a24774fbefb2b84694e38a691dcbe69a911163
Nate Fortner <summit4me@gmail.com>
2021-11-01 09:28:31 -0700

    Updating inputdirector

--- InputDirector/tools/ChocolateyInstall.ps1
+++ InputDirector/tools/ChocolateyInstall.ps1
@@ -1,3 +1,3 @@
 $drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage 'InputDirector' 'https://www.inputdirector.com/downloads/InputDirector.v1.4.zip' $drop
-."$drop/InputDirector.v1.4.build110.Setup.exe" /S
+Install-ChocolateyZipPackage 'InputDirector' 'http://inputdirector.com/downloads/InputDirector.v2.1.zip' $drop
+."$drop/InputDirector.v2.1.build146.Setup.exe" /S
